d2b547c488f04d1a41a799918815dfd126532e8c
BennyBlanco88 <ben.langs@yahoo.com>
2023-11-25 20:00:22 -0800

    checking if logo generated correctly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
 const inquirer = require('inquirer');
-const {Triangle, Circle, Square} = require("./lib/shapes")
+const {Circle, Square, Triangle} = require("./lib/shapes")
 
 class Svg{
     constructor(){
@@ -9,7 +9,9 @@ class Svg{
     }
     render(){
 
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+        return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+        
+        
     }
     setTextElement(text,color){
         this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
@@ -111,3 +113,4 @@ async function init() {
      writeToFile(svg_file, svgString);
 }
 init()
+
